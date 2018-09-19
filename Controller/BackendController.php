0aa900f66b33c023c37822d802181f53abcfbee3
Dennis Eichhorn <spl1nes.com@googlemail.com>
2018-09-19 21:34:04 +0200

    Split controllers per application

--- Controller/BackendController.php
+++ Controller/BackendController.php
@@ -12,7 +12,7 @@
  */
 declare(strict_types=1);
 
-namespace Modules\Knowledgebase;
+namespace Modules\Knowledgebase\Controller;
 
 use phpOMS\Message\RequestAbstract;
 use phpOMS\Message\ResponseAbstract;
@@ -42,57 +42,8 @@ use Modules\Knowledgebase\Models\PermissionState;
  * @link       http://website.orange-management.de
  * @since      1.0.0
  */
-final class Controller extends ModuleAbstract implements WebInterface
+class BackendController extends Controller
 {
-
-    /**
-     * Module path.
-     *
-     * @var string
-     * @since 1.0.0
-     */
-    public const MODULE_PATH = __DIR__;
-
-    /**
-     * Module version.
-     *
-     * @var string
-     * @since 1.0.0
-     */
-    public const MODULE_VERSION = '1.0.0';
-
-    /**
-     * Module name.
-     *
-     * @var string
-     * @since 1.0.0
-     */
-    public const MODULE_NAME = 'Knowledgebase';
-
-    /**
-     * Module id.
-     *
-     * @var int
-     * @since 1.0.0
-     */
-    public const MODULE_ID = 1005900000;
-
-    /**
-     * Providing.
-     *
-     * @var string[]
-     * @since 1.0.0
-     */
-    protected static $providing = [];
-
-    /**
-     * Dependencies.
-     *
-     * @var string[]
-     * @since 1.0.0
-     */
-    protected static $dependencies = [
-    ];
 
     /**
      * @param RequestAbstract  $request  Request
@@ -275,118 +226,5 @@ final class Controller extends ModuleAbstract implements WebInterface
         $view->setData('category', new NullWikiDoc());
 
         return $view;
-    }
-
-    public function apiWikiDocCreate(RequestAbstract $request, ResponseAbstract $response, $data = null) : void
-    {
-        if (!empty($val = $this->validateWikiDocCreate($request))) {
-            $response->set('wiki_doc_create', new FormValidation($val));
-
-            return;
-        }
-
-        $doc = $this->createWikiDocFromRquest($request);
-        WikiDocMapper::create($doc);
-        $response->set('doc', $doc->jsonSerialize());
-    }
-
-    public function createWikiDocFromRquest(RequestAbstract $request) : WikiDoc
-    {
-        $mardkownParser = new Markdown();
-
-        $doc = new WikiDoc();
-        $doc->setName((string) $request->getData('title'));
-        $doc->setDoc((string) $request->getData('plain'));
-        $doc->setCategory((int) $request->getData('category'));
-        $doc->setStatus((int) $request->getData('status'));
-
-        return $doc;
-    }
-
-    private function validateWikiDocCreate(RequestAbstract $request) : array
-    {
-        $val = [];
-        if (($val['title'] = empty($request->getData('title')))
-            || ($val['plain'] = empty($request->getData('plain')))
-            || ($val['category'] = empty($request->getData('category')))
-            || ($val['status'] = (
-                $request->getData('status') !== null
-                && !WikiStatus::isValidValue((int) $request->getData('status'))
-            ))
-        ) {
-            return $val;
-        }
-
-        return [];
-    }
-
-    public function apiWikiCategoryCreate(RequestAbstract $request, ResponseAbstract $response, $data = null) : void
-    {
-        if (!empty($val = $this->validateWikiCategoryCreate($request))) {
-            $response->set('wiki_category_create', new FormValidation($val));
-
-            return;
-        }
-
-        $category = $this->createWikiCategoryFromRquest($request);
-        WikiCategoryMapper::create($category);
-        $response->set('category', $category->jsonSerialize());
-    }
-
-    public function createWikiCategoryFromRquest(RequestAbstract $request) : WikiCategory
-    {
-        $mardkownParser = new Markdown();
-
-        $category = new WikiCategory();
-        $category->setName((string) $request->getData('title'));
-        $category->setParent((int) $request->getData('parent'));
-
-        return $category;
-    }
-
-    private function validateWikiCategoryCreate(RequestAbstract $request) : array
-    {
-        $val = [];
-        if (($val['title'] = empty($request->getData('title')))
-            || ($val['parent'] = empty($request->getData('parent')))
-        ) {
-            return $val;
-        }
-
-        return [];
-    }
-
-    public function apiWikiBadgeCreate(RequestAbstract $request, ResponseAbstract $response, $data = null) : void
-    {
-        if (!empty($val = $this->validateWikiBadgeCreate($request))) {
-            $response->set('wiki_badge_create', new FormValidation($val));
-
-            return;
-        }
-
-        $badge = $this->createWikiBadgeFromRquest($request);
-        WikiBadgeMapper::create($badge);
-        $response->set('badge', $badge->jsonSerialize());
-    }
-
-    public function createWikiBadgeFromRquest(RequestAbstract $request) : WikiBadge
-    {
-        $mardkownParser = new Markdown();
-
-        $badge = new WikiBadge();
-        $badge->setName((string) $request->getData('title'));
-
-        return $badge;
-    }
-
-    private function validateWikiBadgeCreate(RequestAbstract $request) : array
-    {
-        $val = [];
-        if (($val['title'] = empty($request->getData('title')))
-        ) {
-            return $val;
-        }
-
-        return [];
     }
 }
