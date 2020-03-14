03a22a8eef1e45a5a4d3d01adf12bb721358a4c1
Dennis Eichhorn <spl1nes.com@googlemail.com>
2020-03-14 14:27:06 +0100

    fix namespace

--- tests/Admin/AdminTest.php
+++ tests/Admin/AdminTest.php
@@ -12,7 +12,7 @@
  */
 declare(strict_types=1);
 
-namespace Modules\tests\Knowledgebase\Admin;
+namespace Modules\Knowledgebase\tests\Admin;
 
 /**
  * @internal
--- tests/Models/WikiAppTest.php
+++ tests/Models/WikiAppTest.php
@@ -12,7 +12,7 @@
  */
 declare(strict_types=1);
 
-namespace Modules\tests\Knowledgebase\Models;
+namespace Modules\Knowledgebase\tests\Models;
 
 use Modules\Knowledgebase\Models\WikiApp;
 
