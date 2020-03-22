5161ad9e233aaacce3891cbdb562ef97d022c9a0
Dennis Eichhorn <spl1nes.com@googlemail.com>
2020-03-22 18:42:34 +0100

    implement localization

---
+++ Theme/Backend/Lang/de.lang.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Orange Management
+ *
+ * PHP Version 7.4
+ *
+ * @package   Modules\Knowledgebase
+ * @copyright Dennis Eichhorn
+ * @license   OMS License 1.0
+ * @version   1.0.0
+ * @link      https://orange-management.org
+ */
+declare(strict_types=1);
+
+return ['Knowledgebase' => [
+    'Categories' => 'Kategorien',
+    'Name'       => 'Name',
+    'Parent'     => 'Übergeordnet',
+    'Wiki'       => 'Wiki',
+]];
