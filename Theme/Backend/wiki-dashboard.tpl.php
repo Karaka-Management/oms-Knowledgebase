9503aadbd7eaeb34cfd8419cc72177fe5cb0a161
Dennis Eichhorn <spl1nes.com@googlemail.com>
2020-06-25 22:07:17 +0200

    use portlets

--- Theme/Backend/wiki-dashboard.tpl.php
+++ Theme/Backend/wiki-dashboard.tpl.php
@@ -26,19 +26,21 @@ echo $this->getData('nav')->render(); ?>
     <div class="col-xs-12 col-md-8 col-lg-9">
         <div class="row">
             <?php foreach ($documents as $doc) : $url = UriFactory::build('{/prefix}wiki/doc/single?id=' . $doc->getId()); ?>
-            <div class="portlet">
-                <div class="portlet-head"><a href="<?= $url; ?>"><?= $this->printHtml($doc->getName()); ?></a></div>
-                <div class="portlet-body">
-                    <article>
-                        <?= \substr($doc->getDoc(), 0, 300) . (\strlen($doc->getDoc()) > 300 ? '...' : ''); ?>
-                    </article>
-                </div>
-                <div class="portlet-foot">
-                    <div class="overflowfix">
-                        <?php $tags = $doc->getTags(); foreach ($tags as $tag) : ?>
-                            <span class="tag" style="background: <?= $this->printHtml($tag->getColor()); ?>"><?= $this->printHtml($tag->getTitle()); ?></span>
-                        <?php endforeach; ?>
-                        <a href="<?= $url; ?>" class="button floatRight">More</a>
+            <div class="col-xs-12 plain-grid">
+                <div class="portlet">
+                    <div class="portlet-head"><a href="<?= $url; ?>"><?= $this->printHtml($doc->getName()); ?></a></div>
+                    <div class="portlet-body">
+                        <article>
+                            <?= \substr($doc->getDoc(), 0, 300) . (\strlen($doc->getDoc()) > 300 ? '...' : ''); ?>
+                        </article>
+                    </div>
+                    <div class="portlet-foot">
+                        <div class="overflowfix">
+                            <?php $tags = $doc->getTags(); foreach ($tags as $tag) : ?>
+                                <span class="tag" style="background: <?= $this->printHtml($tag->getColor()); ?>"><?= $this->printHtml($tag->getTitle()); ?></span>
+                            <?php endforeach; ?>
+                            <a href="<?= $url; ?>" class="button floatRight">More</a>
+                        </div>
                     </div>
                 </div>
             </div>
