619a95b4701c48b5a6137b50e1972ed37e11aff4
Dennis Eichhorn <spl1nes.com@googlemail.com>
2021-08-06 19:25:46 +0200

    add dashboard link name

--- Theme/Backend/Lang/Navigation.en.lang.php
+++ Theme/Backend/Lang/Navigation.en.lang.php
@@ -14,6 +14,7 @@ declare(strict_types=1);
 
 return ['Navigation' => [
     'Wiki'       => 'Wiki',
+    'Dashboard'       => 'Dashboard',
     'Categories' => 'Categories',
     'Apps'       => 'Apps',
 ]];
