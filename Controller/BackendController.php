da076b07730b1d4d193e906bd0ceed7cc61f1c36
Dennis Eichhorn <spl1nes.com@googlemail.com>
2022-08-16 19:11:58 +0200

    add css version

--- Controller/BackendController.php
+++ Controller/BackendController.php
@@ -56,7 +56,7 @@ final class BackendController extends Controller
     public function setUpBackend(RequestAbstract $request, ResponseAbstract $response, mixed $data = null) : void
     {
         $head = $response->get('Content')->getData('head');
-        $head->addAsset(AssetType::CSS, '/Modules/Knowledgebase/Theme/Backend/styles.css');
+        $head->addAsset(AssetType::CSS, '/Modules/Knowledgebase/Theme/Backend/styles.css?v=1.0.0');
     }
 
     /**
