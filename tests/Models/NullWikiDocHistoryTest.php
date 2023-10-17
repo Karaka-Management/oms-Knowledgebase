b1daf28ba803d07eb0d920855745babce04b270f
Dennis Eichhorn <spl1nnes.com@googlemail.com>
2023-10-17 00:13:58 +0000

    fix jsonSerialize function call

--- tests/Models/NullWikiDocHistoryTest.php
+++ tests/Models/NullWikiDocHistoryTest.php
@@ -47,6 +47,6 @@ final class NullWikiDocHistoryTest extends \PHPUnit\Framework\TestCase
     public function testJsonSerialize() : void
     {
         $null = new NullWikiDocHistory(2);
-        self::assertEquals(['id' => 2], $null);
+        self::assertEquals(['id' => 2], $null->jsonSerialize());
     }
 }
